04ae2fb878b8439b2f56c809f6953acfe7c9915c
Gbemmiii <62169659+Gbemmiii@users.noreply.github.com>
2020-06-02 17:56:55 +0100

    update

--- purchase.py
+++ purchase.py
@@ -2,3 +2,10 @@ first_Name = input("Please enter your first name \n")
 last_Name = input("Please enter your last name \n")
 message = "Hello" +" " + first_Name
 print(message)
+
+
+sale = int(input("Enter the coresponding number \n 1. Wholesale \n 2. Retail \n"))
+if sale == 1:
+    print("Welcome to the warehouse")
+else:
+    print("Maximum purchase quantity per customer is 30")   
